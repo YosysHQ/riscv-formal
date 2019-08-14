40c1d8c5ba74a81784723665b1ada2ab032d7934
Olof Kindgren <olof.kindgren@gmail.com>
2019-08-14 22:45:23 +0200

    serv: Fix source fetching script

--- cores/serv/generate.sh
+++ cores/serv/generate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ex
 rm -rf serv-src
-git clone git@github.com:olofk/serv.git serv-src
-sed -i -e '/define RISCV_FORMAL/ d;' serv-src/rtl/serv_top.v
+git clone https://github.com/olofk/serv serv-src
 python3 ../../checks/genchecks.py
