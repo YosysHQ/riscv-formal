aa5cfe8739551ff16ba74f809aff049565fd9ea1
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2025-03-10 10:34:30 +1300

    Bringing sphinx stuff in line

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,19 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
+#!/usr/bin/env python3
 project = 'RISC-V Formal'
-copyright = '2024, YosysHQ GmbH'
+copyright = '2025, YosysHQ GmbH'
 author = 'YosysHQ GmbH'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+# select HTML theme
+html_theme = 'furo-ys'
+html_css_files = ['custom.css']
+html_theme_options: dict[str, any] = {}
 
-extensions = []
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'furo'
+# These folders are copied to the documentation's HTML output
 html_static_path = ['_static']
+
+# generate section labels from their heading
+extensions = ['sphinx.ext.autosectionlabel']
+
+# ensure that autosectionlabel will produce unique names
+autosectionlabel_prefix_document = True
+autosectionlabel_maxdepth = 1
