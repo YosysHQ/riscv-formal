a205040340749630c6281f4a779995877294648b
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2025-11-05 09:20:50 +1300

    insns.wrapped_model updates
    Some typing changes to fix typeerrors.
    Don't overload `op_values`, use separate `op_values_list` instead.
    Fallback to `Instruction._v_insn_map()` for wrapping a single instruction.
    Removing xlen as an argument.
    Raising `NotImplementedError` on bad fields (mostly to keep type checker happy, not sure how many are actually possible in practice, at least when using the sail configurator).

--- cli/riscv_formal/insns/wrapped_model.py
+++ cli/riscv_formal/insns/wrapped_model.py
@@ -8,7 +8,7 @@ from .model import Instruction
 class WrappedInstruction(Instruction):
     checker_module: str
 
-    inst_args: Optional[list[str]] = None
+    inst_args: list[str] = field(default_factory=list)
 
     wrap_x_in: bool = False
     wrap_x_out: bool = False
@@ -18,14 +18,14 @@ class WrappedInstruction(Instruction):
     x_lower: int = 1
     r_bits: int = 5
 
-    extra_sig1: list[tuple[str, str, str]] = field(default_factory=list)
-    extra_sig2: list[tuple[str, str, str]] = field(default_factory=list)
-    op_name: str | list[str] = "op"
+    extra_sig1: list[tuple[str, str, Optional[str]]] = field(default_factory=list)
+    extra_sig2: list[tuple[str, str, Optional[str]]] = field(default_factory=list)
+    op_name: str | list[str] | None = "op"
     # [mnemonic, enum_value, op_value]
-    op_values: list[tuple[str, str | list[str], str | list[str]]] = field(default_factory=list)
+    op_values_list: list[tuple[str, str | list[str], str | list[str]]] = field(default_factory=list)
 
-    op_type_enum: Optional[str | list[str]] = None
-    op_value_switch: str = ""
+    op_type_enum: str | list[str] | None = None
+    op_value_switch: Optional[str] = None
 
     def _inputs_used(self):
         inputs = super()._inputs_used()
@@ -45,13 +45,13 @@ class WrappedInstruction(Instruction):
         for inst_arg in self.inst_args:
             for arg_part in inst_arg.split():
                 if arg_part not in inst_args_avail and not arg_part[0].isdigit():
-                    raise NotImplementedError()
+                    raise NotImplementedError(arg_part)
 
-    def _v_reg_wrap(self, xlen: int) -> str:
+    def _v_reg_wrap(self) -> str:
         # wrap registers
-        x_type = f"logic [{xlen-1}:0]"
+        x_type = f"logic [{self.xlen-1}:0]"
         x_range = f"[{self.x_upper}:{self.x_lower}]"
-        reg_wrap = "// register wrapping\n"
+        reg_wrap = ""
         for x_inout, do_wrap in [("x_in", self.wrap_x_in), ("x_out", self.wrap_x_out)]:
             if do_wrap:
                 reg_wrap += f"{x_type} {x_inout}{x_range};\n"
@@ -67,7 +67,7 @@ class WrappedInstruction(Instruction):
             if used_reg in self._maybe_sources and self.wrap_x_in:
                 x_assigns.append(f"assign x_in[{raddr}] = rvfi_{used_reg}_rdata;")
             if used_reg in self._maybe_dests and self.wrap_x_out:
-                result_decl = f"wire [{xlen-1}:0] result = x_out[{raddr}];\n"
+                result_decl = f"wire [{self.xlen-1}:0] result = x_out[{raddr}];\n"
         if localparams:
             localparams = ", ".join(localparams)
             reg_wrap += f"localparam {localparams};\n"
@@ -76,31 +76,48 @@ class WrappedInstruction(Instruction):
         reg_wrap += result_decl
 
         if self.wrap_next_pc:
-            reg_wrap += f"wire [{xlen-1}:0] next_pc = rvfi_pc_rdata + 4;\n"
-        return reg_wrap
+            reg_wrap += f"wire [{self.xlen-1}:0] next_pc = rvfi_pc_rdata + 4;\n"
+        if reg_wrap:
+            return "// register wrapping\n" + reg_wrap
+        else:
+            return "// no register wrapping"
 
     def _v_extra_sigs(self) -> str:
         extra_signals = "// extra signals\n"
         extra_signal_decls = self.extra_sig1 + self.extra_sig2
         if isinstance(self.op_name, str):
-            op_default = self.op_values[0][1] if len(self.op_values) == 1 else None
+            if len(self.op_values_list) == 1:
+                op_default = self.op_values_list[0][1]
+                if isinstance(op_default, list):
+                    raise NotImplementedError(op_default)
+            else:
+                op_default = None
+            if not isinstance(self.op_type_enum, str):
+                raise NotImplementedError(self.op_type_enum)
             extra_signal_decls.append((self.op_type_enum, f"{self.op_name}_0", op_default))
         for t, n, v in extra_signal_decls:
             extra_signals += f"{t} {n};\n" if v == None else f"{t} {n} = {v};\n"
         return extra_signals
 
-    def _v_insn_map(self, xlen: int) -> str:
-        v_str = self._v_reg_wrap(xlen) + '\n'
+    def _v_insn_map(self) -> str:
+        v_str = self._v_reg_wrap() + '\n'
         v_str += self._v_extra_sigs() + '\n'
-        # combined instruction mapping
-        insn_map = "// insn check\n"
+        if self.op_name is None:
+            return v_str + super()._v_insn_map()
+        else:
+            # combined instruction mapping
+            insn_map = "// insn check\n"
 
         if isinstance(self.op_name, list):
+            if not isinstance(self.op_type_enum, list):
+                raise NotImplementedError(self.op_type_enum)
             for name, type_enum in zip(self.op_name, self.op_type_enum):
                 insn_map += f"{type_enum} {name}_0;\n"
             
             op_value_keys = self.op_name
         else:
+            if self.op_value_switch is None:
+                raise NotImplementedError(self.op_value_switch)
             op_value_keys = self.op_value_switch.split()
 
         if len(op_value_keys) == 1:
@@ -108,8 +125,8 @@ class WrappedInstruction(Instruction):
         else:
             case_switch = '{' + ', '.join([f"insn_{k}" for k in op_value_keys]) + '}'
         op_value_bits = sum([self._insn_part_dict[k] for k in op_value_keys])
-        if len(self.op_values) == 1:
-            insn_map += f"wire illinsn = {case_switch} != {op_value_bits}'b {self.op_values[0][0]};\n"
+        if len(self.op_values_list) == 1:
+            insn_map += f"wire illinsn = {case_switch} != {op_value_bits}'b {self.op_values_list[0][0]};\n"
         else:
             insn_map += dedent(f"""\
                 reg illinsn;
@@ -117,19 +134,15 @@ class WrappedInstruction(Instruction):
                     illinsn <= 0;
                     case ({case_switch})
             """)
-            # click.echo("Found instructions: ", nl=False)
             if isinstance(self.op_name, list):
-                for mnemonic, part_type_values, part_values in self.op_values:
-                    # click.echo(f"{mnemonic} ", nl=False)
+                for _, part_type_values, part_values in self.op_values_list:
                     var_name = '{' + ', '.join(f"{name}_0" for name in self.op_name) + '}'
                     value = ''.join(part_values)
                     enum = '{' + ', '.join(part_type_values) + '}'
                     insn_map += f"        {op_value_bits}'b {value}: {var_name} <= {enum};\n"
             else:
-                for mnemonic, enum, value in self.op_values:
-                    # click.echo(f"{mnemonic} ", nl=False)
+                for _, enum, value in self.op_values_list:
                     insn_map += f"        {op_value_bits}'b {value}: {self.op_name}_0 <= {enum};\n"
-            # click.echo("")
             insn_map += dedent(f"""\
                         default: illinsn <= 1;
                     endcase
@@ -138,7 +151,7 @@ class WrappedInstruction(Instruction):
 
         return v_str + insn_map
 
-    def _v_instantiation(self, xlen) -> str:
+    def _v_instantiation(self) -> str:
         instantiation = f"// {self.name} instance\n"
         checker_args: list[str] = []
         for inst_arg in self.inst_args:
@@ -176,12 +189,12 @@ class WrappedInstruction(Instruction):
 
         return instantiation
 
-    def _v_spec_value(self, spec_sig: str, xlen: int) -> str:
+    def _v_spec_value(self, spec_sig: str) -> Optional[str]:
         if spec_sig == "pc_wdata" and self.wrap_next_pc:
             return None
         else:
-            return super()._v_spec_value(spec_sig, xlen)
+            return super()._v_spec_value(spec_sig)
 
-    def _v_checks(self, xlen: int):
-        super()._v_checks(xlen)
+    def _v_checks(self):
+        super()._v_checks()
         self._v_inst_check()
