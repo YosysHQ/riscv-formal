c532c7e5745805fa7d5056f8e49e635d9e5a5762
Krystine Sherwin <93062060+KrystalDelusion@users.noreply.github.com>
2025-12-09 11:47:21 +1300

    Proto testbug.cfg generator
    Add generated testbug configs to .gitignore.
    Skip adding a .gitignore to check directories.
    Specify `testbug%` instead of `%` so that the makefile doesn't try to check `%.cfg` for every other file (looking at you `gen_test.py.cfg`).

--- cores/bad_nerv/README.md
+++ cores/bad_nerv/README.md
@@ -6,5 +6,29 @@ it's NERV but broken in specific ways to test riscv-formal
 tesbug0xx
 ---------
 
-These are configuration errors, many of which call ``exit`` in Yosys, which is
-not recognized as a valid command in scripts, producing an SBY ``ERROR`` state.
+These check config parsing.  Not all of these are bugs/errors and may instead
+call ``exit`` in Yosys, which is not recognized as a valid command in scripts,
+producing an SBY ``ERROR`` state.
+
+- 000-002: script-* sections are inserted relative to ``read`` and ``prep``
+- 003-005: defines and script-defines sections can be specified per-check
+
+testbug1xx
+----------
+
+rvfi errors
+
+testbug2xx
+----------
+
+instruction errors
+
+testbug3xx
+----------
+
+csr errors
+
+testbug 4xx
+-----------
+
+memory errors
---
+++ cores/bad_nerv/gen_test.py
@@ -0,0 +1,66 @@
+#!/bin/python3
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Iterable
+
+import click
+
+@dataclass(frozen=True)
+class BugCfg:
+    id: int
+    checks: list[str] = field(default_factory=list)
+
+    @property
+    def id_str(self) -> str:
+        return f"{self.id:03d}"
+
+    @property
+    def name(self) -> str:
+        return f"testbug{self.id_str}"
+
+
+BUGS = [
+    BugCfg(101, ["reg"]),
+]
+
+def get_bugs(testbug: str) -> Iterable[BugCfg]:
+    for bug in BUGS:
+        if testbug in [bug.id_str, bug.name, "all"]:
+            yield bug
+
+@click.command
+@click.argument("testbug", default="all")
+def run(testbug: str):
+    """
+    Generate the riscv-formal <TESTBUG>.cfg (e.g. "testbug101" or "101").
+    Use "all" to generate all available.
+    """
+    with open("checks.cfg", "r") as checks_cfg:
+        base_cfg = checks_cfg.readlines()
+
+    valid_bug = False
+    for bug in get_bugs(testbug):
+        with open(f"{bug.name}.cfg", "w") as bug_cfg:
+            click.echo(f"Writing to {bug_cfg.name}")
+            for line in base_cfg:
+                click.echo(line, bug_cfg, nl=False)
+                if line.startswith("[defines]"):
+                    # enable testbug
+                    click.echo(f"`define NERV_TESTBUG_{bug.id_str}", bug_cfg)
+
+            if bug.checks:
+                click.echo("", bug_cfg)
+                click.echo("[filter-checks]", bug_cfg)
+                for check in bug.checks:
+                    click.echo(f"+ {check}", bug_cfg)
+                click.echo("- .*", bug_cfg)
+
+        valid_bug = True
+
+    if not valid_bug:
+        click.echo(f"{testbug!r} did not match any known config!", err=True)
+        exit(1)
+
+if __name__ == "__main__":
+    run()
